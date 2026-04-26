a417d262a5270a43913203338c04f2b6da78c60c
m on liddle5 <m@liddle5>
2026-04-27 07:52:01 +1200

    0.0.7-4 conf?

--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,7 @@ release = '0.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-
-
-# extensions = []
-
-extensions = [
-    'sphinx.ext.githubpages',
-    # ... other extensions
-]
-
-#
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
